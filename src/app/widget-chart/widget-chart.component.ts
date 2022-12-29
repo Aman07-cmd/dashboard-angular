6ab17af35cbb2e4ae3f347462cc2528312e175ca
Aman07-cmd <amangroot7@gmail.com>
2022-12-29 22:23:50 +0530

    dashboard

---
+++ src/app/widget-chart/widget-chart.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+@Component({
+  selector: 'app-widget-chart',
+  templateUrl: './widget-chart.component.html',
+  styleUrls: ['./widget-chart.component.css']
+})
+export class WidgetChartComponent {
+
+  Highcharts=Highcharts
+  chartOptions={}
+
+  
+}
